4ed1b378187b19ae16dca84fb7e74dd8d747724b
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2025-05-22 10:52:38 -0500

    05_arreglos_asociativos

---
+++ 05_arreglos_asociativos.php
@@ -0,0 +1,33 @@
+<?php
+$course = [
+    'title' => 'Curso de PHP',
+    'subtitle' => 'Aprende PHP desde cero',
+    'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+    'tags' => ['php', 'programación', 'backend'],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $course['title'] ?></title>
+</head>
+<body>
+    <h1>Bienvenido al <?= $course['title'] ?></h1>
+
+    <h2><?= $course['subtitle'] ?></h2>
+
+    <p><?= $course['description'] ?></p>
+
+    <p>
+        <strong>Etiquetas:</strong>
+        <ul>
+            <?php foreach ($course['tags'] as $tag): ?>
+                <li><?= $tag ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </p>
+</body>
+</html>
