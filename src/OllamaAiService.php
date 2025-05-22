f234a2f34b60e0195617b255062357b7d3b35cfd
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2025-05-22 11:19:49 -0500

    OllamaAiService: PHP y un modelo local

---
+++ src/OllamaAiService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use ArdaGnsrn\Ollama\Ollama;
+
+class OllamaAiService 
+{
+    protected $client;
+    
+    public function __construct() 
+    {
+        $this->client = Ollama::client();
+    }
+
+    public function getResponse(string $input): string 
+    {
+        $result = $this->client->chat()->create([
+            'model' => 'deepseek-r1:1.5b',
+            'messages' => [
+                ['role' => 'user', 'content' => $input],
+            ],
+        ]);
+
+        return $result->message->content;
+    }
+}
