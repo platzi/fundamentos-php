2c0caee490b97932600eb47c07a9e07b39855832
Italo Morales Fantone <profesoritalomoralesf@gmail.com>
2025-05-22 11:40:23 -0500

    Inyección de dependencias

---
+++ src/Chat.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class Chat
+{
+    public function __construct(
+        protected FakeAiService $aiService
+    ) {
+        // ...
+    }
+
+    public function start()
+    {
+        echo 'Ask anything to AI' . PHP_EOL;
+
+        while (true) {
+            $input = readline('> ');
+
+            if ($input === 'exit' || $input === '') {
+                break;
+            }  
+
+            $response = $this->aiService->getResponse($input);
+
+            echo $response . PHP_EOL;
+        }
+    }
+}
